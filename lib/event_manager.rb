914166dbdf209f86e6e029d0d0712ed165ba7ef8
Lance <dogmandevices@gmail.com>
2024-04-09 16:35:05 -0500

    compute average sign up hour and time

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,7 +3,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
-
+require 'date'
+require 'pry-byebug'
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -43,6 +44,10 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def peak_times(dates)
+  
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -54,13 +59,26 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+
+mins = []
+hrs = []
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_numbers(row[:homephone])
+  reg_date = row[:regdate]
+
   legislators = legislators_by_zipcode(zipcode)
+
+  reg_time = reg_date.split[1]
+  hrs.push(reg_time.split(':')[0].to_i)
+  mins.push(reg_time.split(':')[1].to_i)
 
   form_letter = erb_template.result(binding)
   save_thank_you_letter(id, form_letter)
 end
+
+avg_hr = hrs.sum(0.0) / hrs.length
+avg_min = mins.sum(0.0) / mins.length
+puts "Peak registration time: #{avg_hr.round}:#{avg_min.round}"
